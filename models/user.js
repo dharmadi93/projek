72e8909fb34b489dc9b289a4f0c7ca71951ddee6
dharmadi93 <dharmadi93@gmail.com>
2016-10-19 09:00:27 +0700

    addded dashboard

--- models/user.js
+++ models/user.js
@@ -1,38 +1,65 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING,
-    address: DataTypes.STRING,
-    phone: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
       },
       
-      addUser: function (user) {
-        User.create({
-          username: user.username,
-          password: user.password,
-          first_name: user.first_name,
-          last_name: user.last_name,
-          email: user.email,
-          role: user.role,
-          address: user.address,
-          phone: user.phone
-        }).then(function (err) {
-          console.log(err.message)
-        })
-      },
+      // addUser: function (user) {
+      //   User.create({
+      //     username: user.username,
+      //     password: user.password,
+      //     first_name: user.first_name,
+      //     last_name: user.last_name,
+      //     email: user.email,
+      //     role: user.role,
+      //     address: user.address,
+      //     phone: user.phone
+      //   }).then(function (err) {
+      //     console.log(err.message)
+      //   })
+      // },
       
-      viewUser: function () {
-        
-      }
+      // viewUser: function () {
+      //
+      // }
     }
   });
   return User;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models')
+var Users = models.User
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/login', function (req, res, next) {
+  // res.send(req.body.username)
+  Users.findOne({
+    where: {
+      username: req.body.username,
+      password: req.body.password
+    }
+  }).catch(function (err) {
+    console.log(err.message)
+  }).then(function (user) {
+    try {
+      if(user.username) {
+        res.redirect('/dashboard')
+      }
+    }catch (err) {
+      res.redirect('/')
+    }
+  })
+
+
+})
+
+router.post('/register', function (req, res, next) {
+  Users.create({
+    first_name: req.body.firstname,
+    last_name: req.body.lastname,
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+    role: req.body.role,
+    address: req.body.address,
+    phone: req.body.phone
+  }).catch(function (err) {
+    console.log(err.message)
+  }).then(res.redirect('/'))
+})
 
 module.exports = router;
